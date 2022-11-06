641370c7541f5d1f6921a73ea76652495150d0e9
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-11-05 19:05:34 -0500

    Initial Commit

---
+++ ToggleApp/ToggleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleAppApp.swift
+//  ToggleApp
+//
+//  Created by Tech Fitco on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ToggleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
